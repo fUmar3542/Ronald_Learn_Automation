a4b270ae39be0db94e2fb107b825a70ab0ecd4f8
Muhammad Umar Farooq <fumar3542@gmail.com>
2024-08-06 16:25:38 +0500

    Aug 06, 2024 Lecture

---
+++ Aug 06, 2024/test.py
@@ -0,0 +1,52 @@
+
+
+# python --version -> python version
+
+# python main.py -> In order to run the file
+
+# print("hello world")
+
+# Multi line print statements
+
+# print("Hello")
+# print("World")
+
+# Single optimized statement
+
+# print("Hello", "World")
+
+# Multi line code
+# line 1
+# line 2
+# line 3
+
+# Variables types
+
+# 1) String
+# 2) Integer
+# 3) Float or double
+# 4) Bool
+
+
+# String
+
+print("Hello World")
+
+name = "Hello World"
+
+print(name)
+
+
+# Naming of variable
+
+# Snake Case
+name = "Name"
+
+print(name)
+
+sentence = "It's a sentence"
+
+# sentence = 'It's a sentence
+
+
+
---
+++ main.py
@@ -0,0 +1,9 @@
+
+# Google App script we use
+# 1) Logger.lor or console.log
+
+
+# In python
+# print()
+
+print("Hello World")
