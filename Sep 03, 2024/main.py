4e4644ad40bf0c9a591a51c7d744b3992e403880
Muhammad Umar Farooq <fumar3542@gmail.com>
2024-09-03 16:26:16 +0500

    Sep 03, 2024 Lecture

---
+++ Sep 03, 2024/main.py
@@ -0,0 +1,24 @@
+
+
+
+# Switch Statement, python -> match case
+
+language = input("Enter a programming language name: ")
+
+if language == 'Python':
+    print("You'll be learning automations")
+elif language == "Javascript":
+    print("You'll be working on web applications")
+else:
+    print("You'll not be learning automations or web application development")
+
+
+# match language:
+#     case "Python":
+#         print("You'll be learning automations")
+#     case "Javascript":
+#         print("You'll be working on web applications")
+#     case _:
+#         print("You'll not be learning automations or web application development")
+
+
