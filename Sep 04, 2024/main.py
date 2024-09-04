ce0b614c9ffef86325256ba8d36d261e71d6dae7
Muhammad Umar Farooq <fumar3542@gmail.com>
2024-09-04 16:28:31 +0500

    Sep 04, 2024 Lecture

---
+++ Sep 04, 2024/main.py
@@ -0,0 +1,43 @@
+
+# String functions
+
+
+# Length
+
+# first_name = "Umar"
+#
+# length = len(first_name)   # Argument passing to function
+#
+# print(length)
+
+# indexing
+
+# 0   1  2
+# a   b  c
+# v1 v2 v3
+
+# 0 -> a, 1 -> b, 2 -> c
+
+# Index range -> 0 to (length of variable - 1)
+
+first_name = "Umar"
+
+# 0 1 2 3
+# U m a r
+
+# # Write a program to print the index range of the input given by User
+#
+# name = input("Enter you name: ")
+# length = len(name)
+#
+# print("0 to "+ str(length-1))
+
+
+# print(first_name[0])
+
+length =  len(first_name)
+end_index = length - 1
+
+print(first_name[end_index])
+
+print(first_name[len(first_name) - 1])
