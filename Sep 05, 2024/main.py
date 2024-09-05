9b23f9b799c9bf8d457408e758993b63df9d87a2
Muhammad Umar Farooq <fumar3542@gmail.com>
2024-09-05 16:28:09 +0500

    Sep 05, 2024 Lecture

---
+++ Sep 05, 2024/main.py
@@ -0,0 +1,43 @@
+
+
+# String built in functions
+
+# Length
+# indexing
+
+
+# ***********************************************************
+
+# slicing
+# range
+
+# name = "Muhammad Umar"
+#
+# print(name[3:8])
+
+
+# ***********************************************************
+
+# check substring in string
+
+# Write a program which will check if the input given by user contains "Umar" or not
+
+user_input = input("Enter your name: ")
+key = "Umar"
+
+if key in user_input:
+    print("Success")
+else:
+    print("Failure")
+
+
+# ***********************************************************
+
+# not operator
+# upper
+# lower
+# strip -> rm white spaces
+# replace
+# split
+# concatenation
+# formatting ->f notation
