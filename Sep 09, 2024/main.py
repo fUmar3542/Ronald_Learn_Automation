72a610696634bb5e51b36b21849a9334cad25b54
Muhammad Umar Farooq <fumar3542@gmail.com>
2024-09-09 16:36:07 +0500

    Sep 09, 2024 Lecture

---
+++ Sep 09, 2024/main.py
@@ -0,0 +1,75 @@
+
+
+
+# String functions
+
+# **********************************************
+# not operator
+
+# email = input("Enter your email address: ")
+#
+# if ("@" not in email) or ('.' not in email):
+#     print("Invalid")
+
+# **********************************************
+# upper
+
+# my_name = "Muhammad uMar farooq"
+#
+# print(my_name.upper())
+
+# **********************************************
+# lower
+
+# print(my_name.lower())
+
+# **********************************************
+# capitalize
+
+# print(my_name.capitalize())
+
+# **********************************************
+# strip -> rm white spaces
+
+# name = input("Enter name: ")
+#
+# print(name)
+#
+# name = name.strip()
+#
+# print(name)
+
+# **********************************************
+# replace
+
+# name = name.replace(' ', '_')
+#
+# print(name)
+
+# **********************************************
+# split
+
+# name = (input("Enter your name(Hint: only 2 words separated by a space): "))  # MuhammadUmar
+# name = name.capitalize()
+#
+# # abc@coworking, ABC@coworking
+#
+#
+# space_index = name.find(' ')
+# starting_index = space_index + 1
+#
+# print(name.capitalize()[0:space_index])
+# print(name.capitalize()[starting_index::])
+
+# print(first_word)
+# print(second_word)
+
+# concatenation
+# formatting ->f notation
+
+
+import math
+import random
+
+print(math.sqrt(4))
+print(math.ceil(3.1))
