af176df39453b047089a1c5af9690354e1cb9d36
Muhammad Umar Farooq <fumar3542@gmail.com>
2024-09-13 16:40:41 +0500

    Sep 13, 2024 Lecture

---
+++ Sep 13, 2024/main.py
@@ -0,0 +1,56 @@
+
+# # Array
+# import array
+#
+# my_array = array.array('i', [1,2,3,4,5])
+#
+# print(my_array[0])
+
+
+# # Numpy
+# import numpy as np
+#
+# my_array = np.array('i', [1,2,3,4,5])
+#
+# print(my_array)
+
+
+# ************************************************
+
+# List
+
+# N-Dimensional lists
+
+one_dimentional = [1,2,3,4]                     # list
+print(one_dimentional[1])
+
+two_dimentional = [[1,2,3,4], [5,6,7,8]]        # lists in a list
+print(two_dimentional[1][2])
+
+three_dimentional = [[[1,2,3,4], [1,2,3,4], [5,6,7,8]], [[1,2,3,4], [5,6,7,8]]]    # lists in lists in a list
+print(three_dimentional[0][1][0])
+
+# 1 2 3 4
+# 5 6 7 8
+
+country_list = [[[[], [[["name", "ssn", "phone"], ["name", "ssn", "phone"], ["name", "ssn", "phone"], ["name", "ssn", "phone"]], [], [], [], []]], [[]], [], []], [[]], [[], []] ]
+
+
+# x,y,z
+
+print(country_list)
+
+# print(my_list[0])
+
+
+# Access List Items
+# Change List Items
+# Add List Items
+# Remove List Items
+# Loop Lists
+# List Comprehension
+# Sort Lists
+# Copy Lists
+# Join Lists
+# List Methods
+# List Exercises
