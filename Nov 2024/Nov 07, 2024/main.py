020f165fa16c6421f964561133e78089c345c0c1
Muhammad Umar Farooq <fumar3542@gmail.com>
2024-11-07 17:45:08 +0500

    Nov 07, 2024 Lecture

---
+++ Nov 2024/Nov 07, 2024/main.py
@@ -0,0 +1,105 @@
+# Problem 2: Sum of Digits in a Range
+# Write a program that takes two numbers, start and end, from the user and prints the sum of the digits for each number in that range. Use a for loop to iterate from start to end.
+
+# Example:
+
+# Input: 10 and 12
+# Output:
+# Sum of digits of 10 = 1
+# Sum of digits of 11 = 2
+# Sum of digits of 12 = 3
+
+####################################################
+# Solution
+
+
+quantity = 2
+temp = 0
+check = 0
+single_number_list = []
+double_digit_list = []
+numbers_list = []
+ten_digit_sum = 0
+result = 0
+
+for i in range(quantity):
+    while True:
+        try:
+            user_input = int(input(f"Enter number {i + 1}: "))
+            if user_input > 0:
+                if user_input > temp:
+                    temp = user_input
+                    numbers_list.append(user_input)
+                    break
+                else:
+                    print("Number must be greater than the previous number")
+            else:
+                print(f"Number {i+1} must be greater than 0")
+        except:
+            print("Error: invalid number")
+print("")
+
+
+#############################################################
+# Solution 2
+# Dynamic solution
+
+for num in range(numbers_list[0], numbers_list[-1] + 1):
+    sum_of_digits = 0
+    number = num
+    while number != 0:
+        # Get the last digit
+        mod = number % 10
+        # Add it to the sum of digits
+        sum_of_digits = sum_of_digits + mod
+        # Remove the last digit from the number
+        number = number // 10
+    print(f"Sum of Digit {num} = {sum_of_digits}")
+
+
+#############################################################
+# Solution 1
+# Static solution
+
+# for num in range(numbers_list[0], numbers_list[-1] + 1):
+#     if num <= 9:
+#         single_number_list.append(num)
+#         print(f"Sum of Digit {num} = {num}")
+#     elif num == 10:
+#         ten_digit_sum = 1
+#         double_digit_list.append(ten_digit_sum)
+#         check = 1
+#         print(f"Sum of Digit {num} = {ten_digit_sum}")
+#     elif num > 10:
+#         first_digit = num // 10
+#         last_digit = num % 10
+#         result = first_digit + last_digit
+#         print(f"Sum of Digit {num} = {result}")
+#         double_digit_list.append(result)
+#
+# print("")
+# print(f"Single Digit List: {single_number_list}")
+# if result != 0 and check != 1:
+#     pass
+# else:
+#     print(f"Double Digit List: {double_digit_list}")
+
+
+
+
+#######################################################
+numbers_list = [1882, 89]
+result_list = []
+for number in numbers_list:
+    sum_of_digits = 0
+    while number != 0:
+        # Get the last digit
+        mod = number % 10
+        # Add it to the sum of digits
+        sum_of_digits = sum_of_digits + mod
+        # Remove the last digit from the number
+        number = number // 10
+    result_list.append(sum_of_digits)
+
+
+
