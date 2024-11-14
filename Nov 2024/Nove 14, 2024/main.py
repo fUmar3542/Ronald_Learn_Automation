61c004018172bba58b65c91b7f61509e1d9a262d
Muhammad Umar Farooq <fumar3542@gmail.com>
2024-11-14 17:37:46 +0500

    Nov 14, 2024 Lecture

---
+++ Nov 2024/Nove 14, 2024/main.py
@@ -0,0 +1,29 @@
+
+
+# 1) Username Validator: Write a program that checks if a given username is valid. A valid username should:
+#
+# Be between 5 and 15 characters long,
+# Only contain letters and digits (no special characters),
+# Start with a letter.
+# If the username meets all the conditions, print "Valid username"; otherwise, print "Invalid username."
+
+
+##########################################################
+# Pseudo Code
+
+# Get username as an input from user
+# Apply while loop to validate the user input
+#   If the first character is a letter move ahead, else "Invalid username."
+#   If input is alphanumeric move ahead, else "Invalid username."
+#   If the length of input is between 5 and 15 break the loop, else "Invalid username."
+# Display "Valid username"
+
+
+##################################################################################
+# Quiz question
+##################################################################################
+# Problem statement
+# Write a program to get an integer from the user and display the sum of its digits
+###################################################################################
+
+
