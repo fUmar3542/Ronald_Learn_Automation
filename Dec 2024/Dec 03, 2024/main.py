b5f74f952bdfa798d3755dc80c7abb76b747435e
Muhammad Umar Farooq <fumar3542@gmail.com>
2024-12-03 17:42:19 +0500

    Dec 03, 2024 Lecture

---
+++ Dec 2024/Dec 03, 2024/main.py
@@ -0,0 +1,28 @@
+
+names_list = ['Umar', 'james', 'Bob', 'Ronald', 'johnson', 'Paul', 'Ali']
+
+names_list.sort()
+
+# print(names_list)
+
+string1 = "Muhammad Umar Farooq"
+string2 = "Ronald Johnson"
+
+# ASCII value of 'A' is lesser than teh ASCII value of 'a'
+
+if 'aaab' < 'aaac':
+    print("Yes")
+else:
+    print("No")
+
+print(ord('a'))
+
+print(ord('A'))
+
+
+#####################################################################
+# Write a program to compare two strings according to their ASCII
+# values and print string 1 is greater than string 2 and vice versa.
+#####################################################################
+
+
