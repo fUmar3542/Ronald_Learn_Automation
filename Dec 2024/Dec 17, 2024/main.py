00696fda9dbd63b952100b2af1c2d08652a20804
Muhammad Umar Farooq <fumar3542@gmail.com>
2024-12-17 17:45:11 +0500

    Dec 17, 2024 Lecture

---
+++ Dec 2024/Dec 17, 2024/main.py
@@ -0,0 +1,113 @@
+
+
+##########################################################
+# Set
+##########################################################
+
+# Unordered, unchangeable
+
+
+# Create a set
+
+names_set = {"Umar", "Ronald", "umar"}
+
+# # Access items
+
+print(names_set)
+
+print(names_set)
+
+print(names_set)
+
+
+list = [1,2,3]
+
+# list[1]
+
+# print("umar")
+#
+# if "umar" in names_set:
+#     print("Umar")
+
+
+# Add items
+
+names_set.add("james")
+print(names_set)
+
+names_set.add("james")
+print(names_set)
+
+# Update() function
+
+names1_set = {"Joe", "james", True, 1, False, 0}
+
+names_set.update(names1_set)
+
+print(names_set)
+
+
+print(1 == 1)
+
+
+# Remove items from a set
+
+# 1)
+names_set.pop()
+print(names_set)
+
+# 2)
+names_set.remove("Umar")
+print(names_set)
+
+# 3)
+names_set.discard("umar")
+print(names_set)
+
+
+# Join sets
+
+# 1) update
+
+names1_set = {"Joe", "james", True, 1, False, 0}
+
+names_set.update(names1_set)
+
+print(names_set)
+
+# 2) Union
+
+names_set2 = {"John", "Jill"}
+
+# Must use variable because we are getting the result as a new variable
+# names_set = names_set.union(names_set2)
+
+names_set = names_set | names_set2
+
+print(names_set)
+
+# 1 + 2 + umar -> 1 2 umar
+# {1, 2, 3} union {1, 4, 5} -> {1, 2, 3, 4, 5}
+
+
+
+
+
+# dname
+# int
+
+# int * 3
+
+var = 3
+
+# class -> blueprint or a single unit
+
+# list.append(45)
+#
+# name = |"name"
+# name.call()
+
+
+# -> Basic Programming -> Object oriented Programming  -> Data structures and algorithms
+
+# stack, queue
