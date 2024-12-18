b4fb9e6d24df3d0d5c0d97288522a7b205b7b257
Muhammad Umar Farooq <fumar3542@gmail.com>
2024-12-18 17:44:31 +0500

    Dec 18, 2024 Lecture

---
+++ Dec 2024/Dec 18, 2024/main.py
@@ -0,0 +1,41 @@
+
+# Collections
+
+# Pointer -> variable -> memory address
+
+# --------------------------------------------
+# C++
+# int a = 12;
+# int* pointer = &a;
+# int* ptr = new int(6);
+# int* ptr1 = new int[5];
+# print(*pointer)
+
+# int arr[5] = {1,2,3,4,5};
+# arr[1] = 12;
+# --------------------------------------------
+
+# Dynamic Arrays -> pointer, size, capacity
+
+# List    -> Dynamic Arrays -> Dynamic Size
+# Tuple   -> Static Arrays  ->  We'll not be able to change the size of array
+# Set     -> Hash table     ->  You can access, add or remove the element faster than lists
+
+
+var = 12
+
+list = [1, 3, 4, "umar", 5]
+list.append(10)
+
+
+# Addresses
+# list = a1  = 100 + 4
+# a1 = 1
+# a2 = 3
+# a3 = 4
+# a4 = 5
+
+
+
+
+
