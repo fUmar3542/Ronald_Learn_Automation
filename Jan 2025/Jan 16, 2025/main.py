0aa9eb9ca000085a9c9a97a593133b4d8954e96a
Muhammad Umar Farooq <fumar3542@gmail.com>
2025-01-16 17:04:22 +0500

    Jan 16, 2025 Lecture

---
+++ Jan 2025/Jan 16, 2025/main.py
@@ -0,0 +1,28 @@
+
+
+names = [
+        {"first": [{}], "second": "farooq"},
+        {"second":"farooq", "first":"umar"}
+]
+
+# names = ["umar", "farooq"]
+
+print(names)
+
+names.append({"second":"johnson", "first":"james"})
+
+# p = {"second":"johnson", "first":"james"}
+# names.append(p)
+
+# if type(p) == dict:
+#         names.append(p)
+
+
+print(names)
+
+names.append("Umar")
+
+print(names)
+
+
+
