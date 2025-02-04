b4676dd243bd62cfb239c7f98c3c25d0c9725b53
Muhammad Umar Farooq <fumar3542@gmail.com>
2025-02-04 17:46:01 +0500

    Feb 04, 2025 Lecture

--- Feb 2025/Feb 03, 2025/second_solution_todo_app.py
+++ Feb 2025/Feb 03, 2025/second_solution_todo_app.py
@@ -1,94 +1,4 @@
-# ###############################################################################
-#
-# To-Do List Application
-#
-# Build a to-do list program that:
-# - Allows users to add, remove, and mark tasks as complete.
-# - Displays tasks with their status (completed or pending).
-# - Bonus: Add an option to prioritize tasks and sort them by priority.
-#
-# ###############################################################################
-#
-# # Input
-#
-# To-Do List Options:
-# 1. Add a task
-# 2. Remove a task
-# 3. Mark a task as complete
-# 4. View all tasks
-# 5. Exit
-#
-# Enter your choice: 1
-# Enter the task description: Finish the project report
-# Enter the task priority (High/Medium/Low): High
-#
-#
-# # Output
-#
-# Task added successfully!
-#
-# ###############################################################################
-#
-# # Input
-#
-# To-Do List Options:
-# 1. Add a task
-# 2. Remove a task
-# 3. Mark a task as complete
-# 4. View all tasks
-# 5. Exit
-#
-# Enter your choice: 4
-#
-#
-# # Output
-#
-# Your To-Do List:
-# 1. [ ] Finish the project report (Priority: High)
-# 2. [ ] Buy groceries (Priority: Medium)
-#
-#
-# ###############################################################################
-#
-# # Input
-#
-# To-Do List Options:
-# 1. Add a task
-# 2. Remove a task
-# 3. Mark a task as complete
-# 4. View all tasks
-# 5. Exit
-#
-# Enter your choice: 3
-# Enter the task number to mark as complete: 1
-#
-#
-# # Output
-#
-# Task 'Finish the project report' marked as complete!
-#
-# ###############################################################################
-#
-# # Input
-#
-# To-Do List Options:
-# 1. Add a task
-# 2. Remove a task
-# 3. Mark a task as complete
-# 4. View all tasks
-# 5. Exit
-#
-# Enter your choice: 4
-#
-#
-# # Output
-#
-# Your To-Do List:
-# 1. [Done] Finish the project report (Priority: High)
-# 2. [ ] Buy groceries (Priority: Medium)
-
-
-# Ronald's solution
+# 2nd solution
 
 task_formatted = 0
 user_choice = 0
@@ -100,7 +10,17 @@ task_list = [{"Status": "Completed","Task": "read","Priority": "High"},
 {"Status": "Completed","Task": "run","Priority": "High"},
 {"Status": "In Progress","Task": "paint","Priority": "Low"}]
 
+sorted_task_list = []
+
+check = False
+priority = 0
+status = 0
+task = 0
 priority_list = ["high","medium","low"]
+priority_choice = 0
+count = 1
+remove_task_choice = 0
+index_list = []
 
 while True:
     try:
@@ -127,7 +47,10 @@ while True:
         except:
             print("Error: invalid entry.")
     elif user_choice == 2:
+        index = 0
         for j in range(len(task_list)):
+            index = index + 1
+            index_list.append(index)
             for i in range(0, len(task_list) - j - 1):
                 if task_list[i]["Status"] == "Completed" and task_list[i + 1]["Status"] == "Completed":
                     pass
@@ -141,7 +64,7 @@ while True:
             try:
                 remove_task_choice = int(input("Which task would you like to remove? Enter task number: "))
                 remove_task_choice = remove_task_choice - 1
-                if remove_task_choice < len(task_list):
+                if remove_task_choice < len(index_list):
                     task_display = task_list[remove_task_choice]["Task"]
                     del task_list[remove_task_choice]
                     print(f"You have successfully removed '{task_display}' from your to-do-list")
@@ -151,7 +74,10 @@ while True:
             except:
                 print("Error: invalid number entered. Try again")
     elif user_choice == 3:
+        index = 0
         for j in range(len(task_list)):
+            index = index + 1
+            index_list.append(index)
             for i in range(0, len(task_list) - j - 1):
                 if task_list[i]["Status"] == "Completed" and task_list[i + 1]["Status"] == "Completed":
                     pass
@@ -165,7 +91,7 @@ while True:
             try:
                 mark_task_complete_choice = int(input("Which task you would you like to mark as complete? Enter task number: "))
                 mark_task_complete_choice = mark_task_complete_choice - 1
-                if mark_task_complete_choice < len(task_list):
+                if mark_task_complete_choice < len(index_list):
                     task_list[mark_task_complete_choice]["Status"] = status_dict["completed"]
                     print(task_list[mark_task_complete_choice])
                     print(f"You have successfully marked '{task_list[mark_task_complete_choice]['Task']}' as complete")
@@ -175,7 +101,10 @@ while True:
             except:
                 print("Error: invalid number entered. Try again")
     elif user_choice == 4:
+        index = 0
         for j in range(len(task_list)):
+            index = index + 1
+            index_list.append(index)
             for i in range(0, len(task_list) - j - 1):
                 if task_list[i]["Status"] == "Completed" and task_list[i + 1]["Status"] == "Completed":
                     pass
@@ -192,16 +121,3 @@ while True:
 
 
 # isalnum -> The isalnum() method returns True if all the characters are alphanumeric, meaning alphabet letter (a-z) and numbers (0-9).
-
-
-
-# Enterprise Level TODO Application
-
-# Database
-
-# Querying the database -> Reading task info from the databse -> order by
-# task_list
-
-# Performance
-
-#
