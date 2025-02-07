c1a7433218fc555273addbac1f04609fba280287
Muhammad Umar Farooq <fumar3542@gmail.com>
2025-02-07 17:44:13 +0500

    Feb 07, 2025 Lecture

---
+++ Feb 2025/Feb 07, 2025/main.py
@@ -0,0 +1,90 @@
+
+############################################################################################################
+
+# Function
+# Function is a piece of code which stored i memoory and can be used simple by calling its name.
+
+# Type of programming
+# - Amateur programming -> can also be included
+# 1) Functional programming
+# 2) Object oriented programming
+
+############################################################################################################
+
+# Why is there a need to work with functions?
+
+# Advantages
+# 1) We can use it later by simply calling it with its name.
+# 2) It removes the duplication of code from our program.
+# 3) It makes the code a standardized one.
+# 4) It improves the structure of our program.
+# 5) It is easier to work with functions in a multi file project.
+# 6) It is necessary for the object oriented programming.
+
+# Disadvantages of functions?
+# 1) There is a fixed syntax to create teh functions that's why tt is not easier to implement the functions.
+# 2) In case of recursions, it can increase the time complexity of our program.
+# 3) It distributes the functionality of your program.
+
+
+############################################################################################################
+
+# Types of functions:
+
+# Generically:
+
+# 1) Fixed / Pre defined functions / Built in functions
+# print(), input() etc.
+
+# 2) Dynamic / User defined functions
+# print_name() etc.
+
+# print("Hello")
+# print("World")
+#
+# # ...
+#
+# print("Hello")
+# print("World")
+#
+#
+# # ...
+#
+# print("Hello")
+# print("World")
+
+
+############################################################################################################
+
+# Parts of the functions:
+
+# 1) Function Header
+# 2) Function Body
+
+# Function Header
+
+# def keyword
+# name of the function
+# Parameters
+# Additional things
+
+# Function Body
+
+# Actual code / actual functionality
+# return values
+
+def print_hello():
+    print("Hello")
+
+
+# print_hello()
+# print_hello()
+
+# Types of function according to their specifications
+
+# Function
+
+
+
+
+
