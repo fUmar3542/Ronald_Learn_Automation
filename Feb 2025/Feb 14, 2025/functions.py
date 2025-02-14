20ec204cca2090190556ec498cf9e86a62e1473f
Muhammad Umar Farooq <fumar3542@gmail.com>
2025-02-14 17:47:01 +0500

    Feb 14, 2025 Lecture

--- Feb 2025/Feb 14, 2025/functions.py
+++ Feb 2025/Feb 14, 2025/functions.py
@@ -1,4 +1,5 @@
 import random
+from typing import Tuple
 
 
 # Write a program which will be getting the name and id of the student. write a function for that.
@@ -29,15 +30,51 @@ def get_user_input():
     return name, student_id
 
 
+# name1 -> string, student_id1 -> int
 def validity_check(name1, student_id1):
     if name1 in name_list and 0 < student_id1 < 100:
         return True
     return False
 
 
-name, student_id = get_user_input()
-validity_check(student_id1=student_id, name1=name)
-print("Program complete.")
+def start_program():
+    name, student_id = get_user_input()
+    validity_check(student_id1=student_id, name1=name)
+    print("Program complete.")
 
 
+# if __name__ == "__main__":
+#     start_program()
+
+
+########################################################################################################
+
+# Function Annotations : Type Hints
+
+# In Python
+# Writing the data type of variable with the variable name in the parameters of the function.
+
+# With single value return
+
+# def validity_check1(name1 : str, student_id1 : int) -> bool:
+#     if name1 in name_list and 0 < student_id1 < 100:
+#         return True
+#     return False
+#
+#
+# validity_check1("umar", "12")
+
+
+# With multiple values return
+
+def validity_check1(name1 : str, student_id1 : int) -> Tuple[bool, str]:
+    if name1 in name_list and 0 < student_id1 < 100:
+        return True, name1
+    return False, name1
+
+
+result = validity_check1("umar", "12")
+
+print(type(result))
+print(result)
 
