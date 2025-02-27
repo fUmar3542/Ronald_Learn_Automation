d205b94e055371c7e29f5f6a8ff913c753d4e7fa
Muhammad Umar Farooq <fumar3542@gmail.com>
2025-02-27 16:54:04 +0500

    Feb 27, 2025 Lecture

---
+++ Feb 2025/Feb 27, 2025/recursion.py
@@ -0,0 +1,15 @@
+
+###########################################################################################
+
+# Write a program with 2 solution
+# 1. Non-recursive function
+# 2. Recursive function
+
+# to find the sum of the digits in the integer passed as argument
+
+# e.g function(1234) -> 10
+
+
+###########################################################################################
+
+
