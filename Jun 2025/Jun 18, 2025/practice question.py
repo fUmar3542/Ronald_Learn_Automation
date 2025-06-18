786d805f309d4b25ded7f3826c0ec059381e4a81
fumar3542 <fumar3542@gmail.com>
2025-06-18 15:43:21 +0500

    Practice Question - June 18, 2025

---
+++ Jun 2025/Jun 18, 2025/practice question.py
@@ -0,0 +1,8 @@
+
+
+data = {}
+
+# Write a program in which we will be getting 2 inputs from the user gmail, password.
+# In case if username and passowrd doesn't exist, the user will be signing up to our platform.
+
+
