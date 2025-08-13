c26e48195e4d1b1c63b031f6cbf6a8862cb3bdb4
fumar3542 <fumar3542@gmail.com>
2025-08-13 16:30:11 +0500

    Recursive Functions

---
+++ Aug 2025/Aug 13, 3035/recursive_functions.py
@@ -0,0 +1,58 @@
+
+
+# Recursive Function
+
+# Ronald"Johnson"
+# "Ronald Johnson"
+
+
+# Print the 1 to n natural numbers. n is the parameter.
+
+# def print_number(function):
+#     if number != 10:
+
+
+# # Print numbers from n to 1
+# def print_number(n):
+#     if n == 0:
+#         return
+#     print(n)
+#     print_number(n-1)
+
+
+# Function call -> 20
+# Function call -> 20 - 1
+# Function call -> 19 - 1
+#.
+#.
+#.
+#.
+# Function call -> 1 - 1 -> 0
+
+
+# number = 20
+# print_number(number)
+#
+# print(number)
+
+
+# # Print numbers from 1 to n
+# def print_number(x, n):
+#     if x == (n+1):
+#         return
+#     print(x)
+#     print_number(x+1, n)
+#
+# print_number(1, 20)
+
+
+# HOME WORK
+
+# Print numbers from 1 to n
+def print_number(n):
+    if x == (n+1):
+        return
+    print(x)
+    print_number(x+1, n)
+
+print_number(20)
