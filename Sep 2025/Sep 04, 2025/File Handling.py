9aa0458691756b2473e9348c1c4a62c615056777
fumar3542 <fumar3542@gmail.com>
2025-09-04 15:34:57 +0500

    File Handling - Practice QUestion

---
+++ Sep 2025/Sep 04, 2025/File Handling.py
@@ -0,0 +1,8 @@
+
+
+# We have to create a program in which we will be practicing the file handling of text files.
+# 1) We have to write a 2 books details into the books.txt file
+# 2) Display the data read from the text file books.txt in a predefined format.
+
+# example:
+# book id, book name, author, publish date
