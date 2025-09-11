f777fcf129a5bcfa01ce4dad789068ea69e00064
fumar3542 <fumar3542@gmail.com>
2025-09-11 15:42:42 +0500

    Return Statement, Main Function

---
+++ Sep 2025/Sep 11, 2025/Practice Question.py
@@ -0,0 +1,18 @@
+
+def view_all_students():
+    pass
+
+
+def get_user_input_value():
+    user_choice = 0
+    return user_choice
+
+
+def main():
+    user_choice = get_user_input_value()
+    print(user_choice)
+    if user_choice == 2:
+        view_all_students()
+
+
+main()
